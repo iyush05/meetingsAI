b02172eae28678c9a6d2aab8215b079c1bf98866
Ayush Kannaujiya <ayushkannaujiya@gmail.com>
2025-08-11 14:45:25 +0530

    feat: authentication with socials

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,91 +1,21 @@
-"use client"
-import { Button } from "@/components/ui/button";
-import { Input } from "@/components/ui/input";
-import { authClient } from "@/lib/auth-client";
-import { useState } from "react";
+import { headers } from "next/headers"
+import { redirect } from "next/navigation"
 
-export default function Home() {
-  const { data: session } = authClient.useSession();
+import { auth } from "@/lib/auth"
+import { HomeView } from "@/modules/home/ui/views/home-view"
 
-  const [name, setName] = useState('');
-  const [email, setEmail] = useState('');
-  const [password, setPassword] = useState('');
-  
-  const onSubmit = () => {
-    authClient.signUp.email({
-      email,
-      name,
-      password
-    }, {
-      onError: () => {
-        window.alert("Something went wrong");
-      }, 
-      onSuccess: () => {
-        window.alert("Success");
-      }
-    })
-  }
+const Page = async() => {
+  const session = await auth.api.getSession({
+    headers: await headers(),
+  });
 
-  const onLogin = () => {
-    authClient.signIn.email({
-      email,
-      password
-    }, {
-      onError: () => {
-        window.alert("Something went wrong");
-      }, 
-      onSuccess: () => {
-        window.alert("Success");
-      }
-    })
-  }
-
-  if(session) {
-    return( 
-        <div className="p-4 flex flex-col gap-y-4">
-          <p>Logged In as {session.user.name}</p>
-          <Button onClick={() => authClient.signOut()}>
-            Sign out
-          </Button>
-        </div>
-    )
+  if(!session) {
+    redirect("/sign-in");
   }
 
   return (
-    <div className="flex flex-col gap-y-10">
-      <div className="p-4 flex flex-col gap-y-4">
-        <Input
-          placeholder="Enter Name"
-          value={name}
-          onChange={(e) => setName(e.target.value)}
-        />
-        <Input
-          placeholder="Enter Email"
-          value={email}
-          onChange={(e) => setEmail(e.target.value)}
-        />
-        <Input
-          type="password"
-          placeholder="Enter Password"
-          value={password}
-          onChange={(e) => setPassword(e.target.value)}
-        />
-        <Button onClick={onSubmit}>Create user</Button>
-      </div>
-      <div className="flex flex-col gap-y-4">
-        <Input
-          placeholder="Enter Email"
-          value={email}
-          onChange={(e) => setEmail(e.target.value)}
-        />
-        <Input
-          type="password"
-          placeholder="Enter Password"
-          value={password}
-          onChange={(e) => setPassword(e.target.value)}
-        />
-        <Button onClick={onLogin}>Login</Button>
-      </div>
-    </div>
-    );
+    <HomeView />
+  )
 }
+
+export default Page;
--- src/modules/auth/ui/views/sign-up-view.tsx
+++ src/modules/auth/ui/views/sign-up-view.tsx
@@ -8,6 +8,7 @@ import { useRouter } from "next/navigation";
 import { Input } from "@/components/ui/input";
 import { authClient } from "@/lib/auth-client";
 import { Button } from "@/components/ui/button";
+import { FaGithub, FaGoogle } from "react-icons/fa";
 import { Card, CardContent } from "@/components/ui/card"
 import { Alert, AlertTitle } from "@/components/ui/alert"
 import { 
@@ -56,11 +57,33 @@ export const SignUpView = () => {
                 name: data.name,
                 email: data.email,
                 password: data.password,
+                callbackURL: "/"
             },
             {
                 onSuccess: () => {
                     setPending(false);
                     router.push("/");
+                },
+                onError: ({ error }) => {
+                    setPending(false);
+                    setError(error.message)
+                }
+            },
+        )
+    }
+
+    const onSocial = (provider: 'google' | 'github') => {
+        setError(null);
+        setPending(true);
+
+        authClient.signIn.social(
+            {
+              provider: provider,
+              callbackURL: "/"
+            },
+            {
+                onSuccess: () => {
+                    setPending(false);
                 },
                 onError: ({ error }) => {
                     setPending(false);
@@ -180,19 +203,21 @@ export const SignUpView = () => {
                                 <div className="grid grid-cols-2 gap-4">
                                     <Button 
                                         disabled={pending}
+                                        onClick={() => onSocial("google")}
                                         variant="outline"
                                         type="button"
                                         className="w-full"
                                     >
-                                    Google    
+                                    <FaGoogle />    
                                     </Button>
                                     <Button 
                                         disabled={pending}
+                                        onClick={() => onSocial("github")}
                                         variant="outline"
                                         type="button"
                                         className="w-full"
                                     >
-                                    Github    
+                                    <FaGithub />    
                                     </Button>
                                 </div>
                                 <div className="text-center text-sm">
